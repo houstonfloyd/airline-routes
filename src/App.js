9f99d2c29096d6d388989822588989f7e5264361
Andrew Ubuntu <andrew.houstonfloyd@gmail.com>
2018-04-12 10:46:49 -0400

    display raw routes data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data.js';
+
+//table - map over data, return row(obj is prop)
+  //row - return 3 * td, [0], [1], [2]
+
 
 class App extends Component {
   render() {
@@ -9,11 +14,46 @@ class App extends Component {
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <p></p>
         </section>
+        <DataTable />
       </div>
+    );
+  }
+}
+
+class DataTable extends Component {
+  render() {
+    const rows = data.routes.map((route) => {
+      let { airline, src, dest } = route;
+      return <TableRow airline={airline} src={src} dest={dest} />;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Airline</th>
+            <th>Source Airport</th>
+            <th>Destination Airport</th>
+          </tr>
+        </thead>
+        <tbody>
+          { rows }
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.airline}</td>
+        <td>{this.props.src}</td>
+        <td>{this.props.dest}</td>
+      </tr>      
     );
   }
 }
